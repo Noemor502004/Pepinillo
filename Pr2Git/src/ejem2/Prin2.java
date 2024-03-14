394bc06d211316d348d9c271bb539f3c17d693b5
Noé <No@Porttil-No>
2024-03-14 14:46:33 +0100

    original

---
+++ Pr2Git/src/ejem2/Prin2.java
@@ -0,0 +1,11 @@
+package ejem2;
+
+public class Prin2 {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("línea 1");
+		System.out.println("línea 2");
+	}
+
+}
---
+++ ProyectoGit1/src/paquete1/Principalgit1.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principalgit1 {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("Primera línea");
+		System.out.println("Segunda línea");
+	}
+
+}
